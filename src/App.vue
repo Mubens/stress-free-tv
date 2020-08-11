d8347a013cef93514a4dc2719a621b76c6935876
Mubens <zhuxiaofengzj@qq.com>
2020-08-12 00:03:27 +0800

    slidebar

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="box">
+      <SlideGuide />
+      <div class="content">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './views/Header'
+import SlideGuide from './views/SlideGuide'
+
+export default {
+  components: {
+    Header,
+    SlideGuide
+  }
+}
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+html {
+  font-size: 14px;
+}
+
+a {
+  color: #000000;
+  text-decoration: none;
+}
+
+.box {
+  display: flex;
+  height: calc(100% - 56px);
+  background-color: #f9f9f9;
+}
+.content {
+  flex: 1;
+}
+</style>
---
+++ src/components/SlideItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="section">
+    <template v-for="item in list">
+      <a :href="item.url" class="item" :key="item.title">
+        <i class="icon" :class="item.icon" :key="item.title"></i>
+        <span :key="item.title">{{ item.title }}</span>
+      </a>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.section {
+  padding: 16px 0;
+  border-bottom: 1px solid #e4e4e4;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  height: 40px;
+  padding: 0 24px;
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+
+  .icon {
+    // width: 24px;
+    // height: 24px;
+    // fill: #606060;
+    color: #606060;
+    font-size: 22px;
+  }
+
+  span {
+    padding-left: 20px;
+  }
+}
+</style>
